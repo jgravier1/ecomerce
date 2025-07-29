b076b9ba0f4311f263c3c275a1a3e7ee7fb8d60f
Juan C Gravier <juancamilo-ortega@hotmail.com>
2025-07-29 10:01:33 -0500

    feat: Add Google Services configuration file for Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:ecomerce/config/routes/app_router.dart';
+import 'package:ecomerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
