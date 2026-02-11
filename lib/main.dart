9e12f8319ef1bdb8bfa5dd3e78ca1356fd5cc061
Juan Camilo Gravier Ortega <juancamilo-ortega@hotmail.com>
2026-02-10 20:16:46 -0500

    feat: Integrate Firebase initialization and dotenv support in main application

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:ecomerce/config/routes/app_router.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:ecomerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:ecomerce/l10n/app_localizations.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
